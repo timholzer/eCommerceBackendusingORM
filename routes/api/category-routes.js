fda7a5d2f192b600733014e05b5a81df1bee5b91
Tim Holzer <timiholzer@gmail.com>
2020-12-14 12:13:16 -0700

    got put route to work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,6 +66,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(CategoryData => {
+      if (!CategoryData) {
+        res.status(404).json({message:'No category at this id'});
+        return;
+      }
+      res.json(CategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
